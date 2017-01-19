a9a7cb936ed0c5da82353031c6a3bd0460f78164
Filipior <filipoou@gmail.com>
2017-01-19 03:08:02 +0100

    ciezko

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ $(function () {
 	cover.on("mouseenter", function (event) {
 		$(this).addClass("coverOnHover" );
 	});
-    
+
     // menu hamburger ----------------------------------------
 
 	var menu = $(".hamburgerMenu");
@@ -19,7 +19,28 @@ $(function () {
 
 	});
 
+// rozszerzanie coverów 
 
+	var cover = $(".cover");
+	var logo = $(".logo");
+	var content = $(".xyz");
+	var coverTitle = $(".content");
+
+	cover.on("click", function(event) {
+		cover.addClass("coverMin");
+		cover.removeClass("coverMax");
+		$(this).removeClass("coverMin").addClass("coverMax");
+		content.css("display", "block");
+		// coverTitle.css("display", "none");
+	});
+
+	logo.on("click", function (event) {
+		cover.removeClass("coverMax");
+		cover.removeClass("coverMin");
+		// content.css("display", "none");
+		coverTitle.css("display", "block");
+
+	});
 
 
 
